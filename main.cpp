9f2e99aaaf558e7dd6168d1c5bfade7bd916198a
seojin <comcxx11@gmail.com>
2022-12-31 18:33:07 +0900

    cpp example 200 start!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+int exam001();
+int exam002();
+int exam003();
+int exam004();
+int exam005();
+int exam006();
+int exam007();
+int exam008();
+int exam009();
+int exam010();
+int exam011();
+int exam012();
+
 int main() {
+//    exam001();
+    exam002();
+
+    return 0;
+}
+
+int exam001() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+int exam002() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
